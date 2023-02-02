6a84397b906fbcba5ac09987b45b7939c8346af1
acvetochka <116402791+acvetochka@users.noreply.github.com>
2023-02-02 15:40:47 +0000

    Deploying to gh-pages from @ acvetochka/mimino@f2ad3c4d5ffd4f8f07fb9df962ce4523820e3877 🚀

---
+++ index.a055f74a.js
@@ -0,0 +1,2 @@
+AOS.init({disable:!1,startEvent:"DOMContentLoaded",initClassName:"aos-init",animatedClassName:"aos-animate",useClassNames:!1,disableMutationObserver:!1,debounceDelay:50,throttleDelay:99,offset:120,delay:0,duration:1e3,easing:"ease",once:!1,mirror:!1,anchorPlacement:"top-bottom"});
+//# sourceMappingURL=index.a055f74a.js.map
